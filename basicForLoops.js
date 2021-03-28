36436065ee324ec4f7b07cebc792f08f4928f48a
Yasir Zafar <yzyasir@yahoo.com>
2021-03-27 23:12:52 -0400

    started another, think it through

--- basicForLoops.js
+++ basicForLoops.js
@@ -75,3 +75,7 @@ for (i=2018; i>0; i--){
 
 // 6) Flexible Counter - Set three variables: lowNum, highNum, mult. Starting at lowNum and going through highNum, print only the integers that are a multiple of mult. 
 //    For example, if lowNum=2, highNum=9, and mult=3, the loop should print 3, 6, 9 (on successive lines)
+
+var lowNum = 0;
+var highNum = 0;
+var mult = 0;
